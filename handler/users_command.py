df01e65fcaa975ad042c0d5e4abf063bec06027e
uni123234 <136456996+uni123234@users.noreply.github.com>
2023-11-23 22:16:56 +0300

    Update users_command.py

--- handler/users_command.py
+++ handler/users_command.py
@@ -87,7 +87,7 @@ async def tests_phrase(call_back: types.CallbackQuery, state: FSMContext):
     translation = random_phrase["translation_phrase"]
     await state.set_state(FSMTest.translation)
     await state.update_data(translation=phrase)
-    await call_back.message.answer("📜Тестуваня буде в виді \n вам буде відправлятися текст він\n а ви його маєте перевести📜")
+    await call_back.message.answer("📜Тестуваня буде в виді \n вам буде відправлятися текст \n а ви його маєте перевести📜")
     await call_back.message.answer(translation)
 
 
